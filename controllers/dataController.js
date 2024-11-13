a61e091dceb9c5a1216f46c420c3e09ee25aefa8
Aaron Cheng <aircondi5@gmail.com>
2024-11-13 18:16:12 -0500

    update getArticle to use newsapi.org

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -1,40 +1,31 @@
 const axios = require('axios');
-url = 'https://eventregistry.org/api/v1/article/getArticles'
+const apiKey = '77275bc14f64499786d72ef46caab9ae';
+url = 'https://newsapi.org/v2/top-headlines';
 
 const getRecentArticles = async (req, res) => {
     const dbClient = req.app.get('dbClient');
-    axios.post(url, {
-      action: 'getArticles',
-      keyword: "Tesla Inc",
-      ignoreSourceGroupUri: "paywall/paywalled_sources",
-      articlesPage: 1,
-      articlesCount: 100,
-      articlesSortBy: "date",
-      articlesSortByAsc: false,
-      dataType: [
-          "news",
-          "pr"
-      ],
-      forceMaxDataTimeWindow: 31,
-      resultType: "articles",
-      lang: "eng",
-      apiKey: "9221d1f8-dc6b-4395-8691-af0db1e5d9a5"
+    axios.get(url, {
+        params: {
+            apiKey: apiKey,
+            country: 'us',
+            pageSize: 100,
+            page: 1
+        }
     })
     .then(async (response) => {
         resp = response.data.articles;
-        ls = resp.results;
         console.log(resp);
-        // row = ls[0];
-        // console.log(row['title'], row['url'], row['body'], row['image']);
-        for (const row of ls) {
-            text = 'INSERT INTO articles (article_name, article_header, article_body, cover_image_url) VALUES ($1, $2, $3, $4) RETURNING *';
-            values = [row['title'], row['url'], row['body'], row['image']];
+        for (const row of resp) {
+            text = 'INSERT INTO articles (article_name, article_url, article_desc, cover_image_url) VALUES ($1, $2, $3, $4) RETURNING *';
+            desc = row['description'];
+            if (desc == null) desc = "";
+            values = [row['title'], row['url'], desc, row['urlToImage']];
             const result = await dbClient.query(
                 text, 
                 values
             );
         }
-        res.status(200).json({ message: 'Login successful', user });
+        res.status(200).json({ message: 'Get recent article success' });
     })
     .catch((err) => console.log(err));
 }
