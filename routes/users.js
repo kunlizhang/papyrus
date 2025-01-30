1c50c897ac73615badacf67093585ca91cc4fd5f
azhou888 <80974913+azhou888@users.noreply.github.com>
2025-01-30 13:21:02 -0500

    user calls

---
+++ routes/users.js
@@ -0,0 +1,28 @@
+// routes/auth.js
+const express = require('express');
+const { registerUser, loginUser, authenticateUser, logoutUser, getUserSavedArticles, addUserInterest, removeUserInterest } = require('../controllers/userController');
+const router = express.Router();
+
+// Route for user registration
+router.post('/register', registerUser);
+
+// Route for user login
+router.post('/login', loginUser);
+
+// Route for user authentication
+router.post('/authenticate', authenticateUser);
+
+// Route for user logout
+router.post('/logout', logoutUser);
+
+// Route for getting user articles
+router.get('/getArticles', getUserSavedArticles);
+
+// Route for adding user interest
+router.post('/addInterest', addUserInterest);
+
+// Route for removing user interest
+router.post('/removeInterest', removeUserInterest);
+
+
+module.exports = router;
